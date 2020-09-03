77d35d2b2e3ac93a4529d43452efacb31011b26c
Christian Zapata <christianzapata@lxbs0037s-mbp.lan>
2020-09-02 22:39:37 -0400

    mistaken export line - fixed

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,4 @@ import createEntry from './createEntry'
 import deleteEntry from './deleteEntry'
 import editEntry from './editEntry';
 
-export { getAllEntries, createEntry, deleteEntry, deleteEntry }
+export { getAllEntries, createEntry, deleteEntry, editEntry }
