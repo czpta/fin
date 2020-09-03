0799ad782a03d25a5a2e6f1fd1b51a60da2c15ea
Christian Zapata <christianzapata@lxbs0037s-mbp.lan>
2020-09-02 22:29:40 -0400

    Cleanup unecessary html

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<DashboardView />, document.getElementById('root2'));
 
 serviceWorker.unregister();
