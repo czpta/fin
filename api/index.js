16ef0e0dc3f5d4bacfb9f5ab12fa7618559794a1
Christian Zapata <christianzapata@lxbs0037s-mbp.lan>
2020-09-02 22:07:17 -0400

    Add export for faunadb api calls

---
+++ api/index.js
@@ -0,0 +1,7 @@
+
+import getAllNotes from './getAllNotes'
+import createNote from './createNote'
+import deleteNote from './deleteNote'
+import editNote from './editNote';
+
+export { getAllNotes, createNote, deleteNote, editNote }
